4d2c323ceaaac1a2d5279cc1d6ed0918d12af409
Meghan <mmhade@gmail.com>
2015-03-16 17:54:16 -0700

    query stops by operator name working

--- public/js/entities/stop.js
+++ public/js/entities/stop.js
@@ -6,7 +6,9 @@ DeveloperPlayground.Stops = Backbone.Collection.extend({
 	model: DeveloperPlayground.Stop,
 	setQueryParameters: function(queryParameters) {
 		// this.url = 'http://localhost:4567/api/v1/stops.json?bbox=-122.39893913269043,37.76651662158726,-122.38070011138915,37.77178331201861';
+		this.identifier = queryParameters.identifier;
 		this.url = queryParameters.url;
+        console.log("url: ", this.url);
 	},
 	parse: function(response, xhr) {
 		return response.stops;
