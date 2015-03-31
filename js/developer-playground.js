4e610957c13427b2ba0c021d6cbf0e058305149e
hanbyul-here <hanbyul@nyu.edu>
2015-03-31 12:01:24 -0400

    added nav
    Conflicts:
    	index.html

--- js/developer-playground.js
+++ js/developer-playground.js
@@ -1,3 +1,10 @@
 $(document).ready(function () {
     DeveloperPlayground.startQueryBuilderView = new DeveloperPlayground.StartQueryBuilderView();
+    if($(document).innerWidth() > 768){
+      console.log("hellow");
+      fixNavigation();
+    $(window).scroll(function(){
+       fixNavigation();
+      });
+    }
 });
