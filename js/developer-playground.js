4f17c5b7a801380fd9ceb45358e2d02442ca2e77
Meghan <mmhade@gmail.com>
2015-04-01 15:16:26 -0700

    add jquery function to listen to ajax request

--- js/developer-playground.js
+++ js/developer-playground.js
@@ -1,4 +1,12 @@
 $(document).ready(function () {
+	$(document).ajaxStart(function() {
+		// start spinner, add style here
+		console.log("Start spinner here");
+	})
+	$(document).ajaxComplete(function(event, request, settings) {
+		// stop spinner, add style here
+		console.log("Stop spinner here");
+	})
     DeveloperPlayground.startQueryBuilderView = new DeveloperPlayground.StartQueryBuilderView();
     if($(document).innerWidth() > 768){
       console.log("hellow");
