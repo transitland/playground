dcf7a5bdb704a91aed022cb8e814de122bd4ba1e
Meghan <mmhade@gmail.com>
2015-04-22 16:29:12 -0700

    create download view, add buttons to download query data to json or csv, add functionality to download view to download csv or json file

---
+++ js/views/download-bar.js
@@ -0,0 +1,36 @@
+var DeveloperPlayground = DeveloperPlayground || {};
+
+DeveloperPlayground.DownloadView = Backbone.View.extend({
+	el: '#download-bar',
+	
+	template: _.template( $('#download-bar-template').html() ),
+
+	events: {
+        'click .btn' : 'submit',
+    },
+
+	initialize:function(options){
+        this.collection = options.collection;
+	},
+
+	render: function() {
+		renderedHtml = this.template();
+		this.$el.html(renderedHtml);
+		return this;
+	},
+
+	submit: function(event) {
+		url = this.collection.url;
+
+		if (event.target.id == "csv") {
+			url = url.replace(".json", ".csv");
+		} else if (event.target.id == "json") {
+			var blob = new Blob([JSON.stringify(this.collection.toJSON())], {type: "application/json;charset=utf-8"});
+			saveAs(blob, "download.json");
+		}
+
+		window.open(url, '_blank');
+
+	}
+
+ });
