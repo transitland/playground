cb1ede4f7edbc13f78910e34c485203423da8c41
Meghan <mmhade@gmail.com>
2015-05-27 14:06:04 -0700

    add anaytics snippet to html header, add per-page limit to urls

--- js/views/start-query-builder.js
+++ js/views/start-query-builder.js
@@ -115,7 +115,7 @@ DeveloperPlayground.StartQueryBuilderView = Backbone.View.extend({
                 this.nameListView = new DeveloperPlayground.NameListView({collection: collection});
             }
             this.operators.setQueryParameters({
-                    url: API_HOST+'/api/v1/operators.json'
+                    url: API_HOST+'/api/v1/operators.json?per_page=5000'
                 });
             collection.fetch();
             return this;
@@ -125,16 +125,6 @@ DeveloperPlayground.StartQueryBuilderView = Backbone.View.extend({
             $(".btn#san-francisco").addClass("active");
             $(".btn#new-york").show();
             $(".btn#san-francisco").show();
-
-
-
-            // commenting out?
-            // if ('undefined' !== typeof this.locationListView) {
-            //     this.locationListView.close();
-            //     this.locationListView = new DeveloperPlayground.LocationListView();
-            // } else {
-            //     this.locationListView = new DeveloperPlayground.LocationListView();
-            // }
 
             if($("#nameMenu").hasClass("dropdown")) $("#nameMenu").removeClass("dropdown");
         }
@@ -157,13 +147,13 @@ DeveloperPlayground.StartQueryBuilderView = Backbone.View.extend({
             if($parameterSelect.val() == "map view") {
             collection = this.stops;
             this.stops.setQueryParameters({
-                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?bbox='+bounds
+                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?bbox='+bounds+'&per_page=5000'
                 });
             // for search by operator name
             } else if($parameterSelect.val() == "operator") {
                 collection = this.stops;
                 this.stops.setQueryParameters({
-                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?servedBy='+identifier,
+                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?servedBy='+identifier+'&per_page=5000',
                 });
             }
         
@@ -174,7 +164,7 @@ DeveloperPlayground.StartQueryBuilderView = Backbone.View.extend({
             if($parameterSelect.val() == "map view") {
                 collection = this.operators;
                 this.operators.setQueryParameters({
-                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?bbox='+bounds
+                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?bbox='+bounds+'&per_page=5000'
                 });
             } else if($parameterSelect.val() == "name") {
                 this.operators.hideAll();
@@ -190,12 +180,12 @@ DeveloperPlayground.StartQueryBuilderView = Backbone.View.extend({
             if($parameterSelect.val() == "map view") {
                 collection = this.routes;
                 this.routes.setQueryParameters({
-                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?bbox='+bounds
+                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?bbox='+bounds+'&per_page=5000'
                 });
             } else if($parameterSelect.val() == "operator") {
                 collection = this.routes;
                 this.routes.setQueryParameters({
-                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?operatedBy='+identifier,
+                    url: API_HOST+'/api/v1/'+$entitySelect.val()+'.json?operatedBy='+identifier+'&per_page=5000',
                 });
             } else if($parameterSelect.val() == "route number") {
                 collection = this.routes;
