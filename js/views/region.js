426a5457996e37265162a229df82014d6b5117b9
Meghan <mmhade@gmail.com>
2016-02-12 17:49:32 -0800

    remove console.log

--- js/views/region.js
+++ js/views/region.js
@@ -8,7 +8,6 @@ DeveloperPlayground.RegionView = Backbone.View.extend({
 		this.$el.get('metro');
 		renderedHtml = this.template(this.model.toJSON());
 		this.$el.html(renderedHtml);
-		console.log(this.model.get('country') + ", " + this.model.get('state') + ", " + this.model.get('metro'));
 		this.$el.val(this.model.get('metro'));
 		this.$el.text(this.model.get('country') + ", " + this.model.get('state') + ", " + this.model.get('metro'));
 		return this;
